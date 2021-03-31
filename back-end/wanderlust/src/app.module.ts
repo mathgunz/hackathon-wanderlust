f0c422e6947cf6a9a1011e7cb06d2d1be0123493
matheus.graciano <matheus.graciano@audsat.com.br>
2021-03-31 00:39:48 -0300

    asd

--- back-end/wanderlust/src/app.module.ts
+++ back-end/wanderlust/src/app.module.ts
@@ -1,18 +1,17 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AgendasModule } from './agendas/agendas.module';
 import { ClientesModule } from './clientes/clientes.module';
 import { GuiasModule } from './guias/guias.module';
 import { PasseiosModule } from './passeios/passeios.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginModule } from './login/login.module';
-import * as connectionOptions from './ormconfig';
 @Module({
   imports: [
     ClientesModule, 
     PasseiosModule, 
     AgendasModule, 
     GuiasModule,
-    TypeOrmModule.forRoot(connectionOptions),
+    TypeOrmModule.forRoot(),
     LoginModule
   ]
 })
