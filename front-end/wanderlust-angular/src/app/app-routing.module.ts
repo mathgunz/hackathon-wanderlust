f150db08e838684fd69a678660f1c2ba8fe31298
João Victor <joao.victor.zaga@gmail.com>
2021-04-08 23:57:11 -0300

    Create headers route

--- front-end/wanderlust-angular/src/app/app-routing.module.ts
+++ front-end/wanderlust-angular/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AjudarComponent } from './ajudar/ajudar.component';
+import { ContatosComponent } from './contatos/contatos.component';
 import { LoginClienteComponent } from './login-cliente/login-cliente.component';
 import { LoginGuiaComponent } from './login-guia/login-guia.component';
+import { NotificacoesComponent } from './notificacoes/notificacoes.component';
+import { PasseiosComponent } from './passeios/passeios.component';
+import { PerfilClientesComponent } from './perfil-clientes/perfil-clientes.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginClienteComponent },
-  { path: 'login-guia', component: LoginGuiaComponent }
+  { path: 'login-guia', component: LoginGuiaComponent },
+  { path: 'ajuda', component: AjudarComponent },
+  { path: 'perfil', component: PerfilClientesComponent },
+  { path: 'contato', component: ContatosComponent },
+  { path: 'passeios', component: PasseiosComponent },
+  { path: 'notificacoes', component: NotificacoesComponent },
 ];
 
 @NgModule({
--- front-end/wanderlust-angular/src/app/app.module.ts
+++ front-end/wanderlust-angular/src/app/app.module.ts
@@ -7,10 +7,22 @@ import { LoginClienteComponent } from './login-cliente/login-cliente.component';
 import {APP_BASE_HREF} from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { PasseiosComponent } from './passeios/passeios.component';
+import { PerfilClientesComponent } from './perfil-clientes/perfil-clientes.component';
+import { NotificacoesComponent } from './notificacoes/notificacoes.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import { AjudarComponent } from './ajudar/ajudar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginClienteComponent
+    LoginClienteComponent,
+    PasseiosComponent,
+    PerfilClientesComponent,
+    NotificacoesComponent,
+    ContatosComponent,
+    PasseiosComponent,
+    AjudarComponent,
+    NotificacoesComponent,
   ],
   imports: [
     BrowserModule,
