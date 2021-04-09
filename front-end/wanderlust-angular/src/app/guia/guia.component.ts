e0d302872daa1f5029c13c8fceef8931c99d42e9
matheus.graciano <matheus.graciano@audsat.com.br>
2021-04-08 23:07:32 -0300

    colocando tela de cadastro do guia

--- front-end/wanderlust-angular/src/app/guia/guia.component.ts
+++ front-end/wanderlust-angular/src/app/guia/guia.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertService } from '../_services';
 
 @Component({
   selector: 'app-guia',
   templateUrl: './guia.component.html',
   styleUrls: ['./guia.component.scss']
 })
+
 export class GuiaComponent implements OnInit {
+  cadastroGuiaForm = this.formBuilder.group({
+    nome: "",
+    sobrenome:"",
+    endereco:"",
+    cidade: "",
+    dataNascimento:"",
+    telefone:"",
+    numeroCadastur:"",
+    email: "",
+    senha: "",
+  });
 
-  constructor() { }
+  loading = false;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private alertService: AlertService
+  ) {
   }
 
+  ngOnInit() {
+
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+    // stop here if form is invalid
+
+
+
+  }
 }
