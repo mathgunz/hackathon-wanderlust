ce2f5718f9ab004e631ea40f766d8c0129714e44
João Victor <joao.victor.zaga@gmail.com>
2021-04-12 21:19:24 -0300

    Update module

--- front-end/wanderlust-angular/src/app/app.module.ts
+++ front-end/wanderlust-angular/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { PasseiosComponent } from './passeios/passeios.component';
 import { NotificacoesComponent } from './notificacoes/notificacoes.component';
 import { ContatosComponent } from './contatos/contatos.component';
 import { AjudarComponent } from './ajudar/ajudar.component';
-import { GuiaComponent } from './guia';
-import { CadastroPasseioComponent } from './guia/cadastro-passeio-fixo/cadastro-passeio.component';
-import { CadastroPasseioPersonalizadoComponent } from './guia/cadastro-passeio-personalizado/cadastro-passeio-personalizado.component';
-import { HomeGuiaComponent } from './guia/home-guia/home-guia.component';
 import { HomeComponent } from './home/home.component';
 
+import {
+  GuiaComponent,
+  CadastroPasseioComponent,
+  CadastroPasseioPersonalizadoComponent,
+  HomeGuiaComponent
+} from './guia';
 
 import {
   PerfilClientesComponent,
@@ -24,10 +26,13 @@ import {
   CadastroClienteComponent,
   ContatoClienteComponent,
 } from './cliente';
-import { HeadersComponent } from './_components/headers/headers.component';
-import { FooterComponent } from './_components/footer/footer.component';
-import { AlertComponent } from './_components/alert/alert.component';
-import { HeaderSlideComponent } from './_components/header-slide/header-slide.component';
+import {
+  HeadersComponent,
+  FooterComponent,
+  AlertComponent,
+  HeaderSlideComponent
+} from './_components';
+
 
 @NgModule({
   declarations: [
--- front-end/wanderlust-angular/src/app/guia/index.ts
+++ front-end/wanderlust-angular/src/app/guia/index.ts
@@ -1,4 +1,5 @@
 export * from './cadastro/guia.cadastro.component';
 export * from './/cadastro-passeio-fixo/cadastro-passeio.component';
 export * from './cadastro-passeio-personalizado/cadastro-passeio-personalizado.component';
+export * from './home-guia/home-guia.component';
 export * from './login-guia/login-guia.component';
