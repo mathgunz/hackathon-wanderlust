e32c76d7e9641865de6e4756007093ff3d675952
matheus.graciano <matheus.graciano@audsat.com.br>
2021-04-14 23:55:31 -0300

    asd

---
+++ front-end/wanderlust-angular/src/app/cliente/detalhe-passeios-cliente/dtos/detalhe.passeio.dtos.ts
@@ -0,0 +1,15 @@
+export class DetalhesPasseios {
+
+  "nomePasseio"?:string;
+  "descricao":string;
+  "guia":string;
+  "data":string;
+  "duracao":string;
+  "tipopasseio":string;
+  "roteiro":string;
+  "valor":string;
+  "detalhesPasseios":string;
+  "status":string;
+  "proxima":string;
+  "anterior":string;
+}
